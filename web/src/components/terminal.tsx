e70e2d672091a0262e33071674e9340f7a8256e5
samyabrata-maji <samyabratamaji334@gmail.com>
2024-06-30 04:06:49 +0530

    feat: terminal help

--- web/src/components/terminal.tsx
+++ web/src/components/terminal.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState, useCallback } from "react";
 import Xterm from "@/components/xterm";
 import { type Terminal as Xterminal } from "@xterm/xterm";
 import { Socket } from "socket.io-client";
+import React from "react";
 
 type ShellOut = { data: string };
 
@@ -35,19 +36,34 @@ export default function Terminal({ socket }: { socket: Socket }) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [terminal]);
 
+  const aiHelp = [
+    { command: ":ai", description: "Ask ai anything." },
+    { command: ":ai -t <task>", description: "Based on the prompt, generates commands that directly executes in the terminal" },
+    { command: ":ai -f <file>", description: "Provide a file path to include its content as context." },
+    { command: ":ai -d <dir>", description: "Provide a directory path to include all files as context" },
+    { command: ":ai -c", description: "Starts an context-based interactive chat window (type \"exit\" to exit)" },
+  ];
+
   return (
-    <div className="bg-[#d9d9d920] py-2">
+    <div className="mt-4 bg-[#d9d9d920] py-1">
       <Xterm
-        className="mx-2"
+        className="mx-1"
         onInit={onInit}
         onDispose={onDispose}
         onData={onData}
         socket={socket}
-        // onResize={({ rows, cols }) => {
-        //   console.log(rows, cols);
-        //   socket.emit("shell:resize", { rows, cols });
-        // }}
+      // onResize={({ rows, cols }) => {
+      //   console.log(rows, cols);
+      //   socket.emit("shell:resize", { rows, cols });
+      // }}
       />
+      <div className="w-full p-4">
+        <p>Quick Tips:</p>
+        <p className="text-sm">• Type <b><i>nvim</i></b> to use neovim.</p>
+        {aiHelp.map((item, index) => <React.Fragment key={index}>
+          <p className="text-sm">• <b><i>{item.command}</i></b> - {item.description}</p>
+          </React.Fragment>)}
+      </div>
     </div>
   );
 }
